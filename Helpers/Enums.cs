f5350bfd189fb46ca1e3b2a90b2a0ad0eb147312
vistaxpman <lenon_vn3000@yahoo.com>
2014-01-15 13:19:39 +0700

    Create Enums.cs

---
+++ Helpers/Enums.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Helpers
+{
+    public class Enums
+    {
+    }
+
+    public enum UserType
+    {
+        SA = 1,
+        RA = 2
+    }
+}
