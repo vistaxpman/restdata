5b279c2927170d4fc6774e4db23ac4cb57f11c7f
vistaxpman <lenon_vn3000@yahoo.com>
2014-03-16 21:28:42 +0700

    Update and rename notes.rb to notes.sql

---
+++ android/notes.sql
@@ -0,0 +1,67 @@
+============================================ 16 - 03  -2014 =================================================================
+Sqlite 
+login and logout code in android + sqlite
+
+http://www.vogella.com/tutorials/AndroidSQLite/article.html
+http://chariotsolutions.com/blog/post/android-advanced-cursors/
+MembersActivity
+
+http://chintankhetiya.wordpress.com/2013/06/01/sqlite-database-example/
+https://github.com/khetiyachintan/SQLite-Database-Example
+http://www.androidhive.info/2012/01/android-login-and-registration-with-php-mysql-and-sqlite/
+http://www.tutorialspoint.com/android/android_session_management.htm
+http://www.learn2crack.com/2013/08/develop-android-login-registration-with-php-mysql.html
+http://fundroiding.wordpress.com/2012/04/17/complete-database-example/
+http://omayib.com/2013/10/09/tutorial-android-login-dan-logout/
+http://junalontherun.com/2011/11/11/android-basic-login-example-with-shared-preference/
+https://github.com/junal/Android-SharedPreferences/tree/master/SharedPreferences
+https://github.com/panoskrt/android/blob/master/DBAdapter.java
+https://github.com/panoskrt/android/blob/master/DBAdapter.java
+http://anujarosha.wordpress.com/2012/01/06/how-to-update-and-delete-data-from-sqlite-database-in-android/
+http://madushankaperera.wordpress.com/2013/05/14/android-sqlite-database-example/http://sabitlabscode.wordpress.com/2014/01/15/xamarin-android-make-crud-operation-with-sqllite-1/
+
+
+=======================================================================================================================
+
+Android Programming: The Big Nerd Ranch Guide (Big Nerd Ranch Guides)
+Android User Interface Design: Turning Ideas and Sketches into Beautifully Designed Apps (Usability) 
+Android Design Patterns: Interaction Design Solutions for Developers 
+Android for Programmers: An App-Driven Approach (2nd Edition) (Deitel Developer Series)
+Pro Android UI
+Learning Android Application Programming: A Hands-On Guide to Building Android Applications 
+Learn Unity for Android Game Development 
+Learn Java for Android Development 
+Android Best Practices
+Android 4.4 App Development Essentials 
+Smashing Android UI (Smashing Magazine Book Series) by Juhani Lehtimaki (Oct 15, 2012) 
+Unity Android Game Development by Example Beginner's Guide by Thomas Finnegan (Dec 20, 2013) 
+App Design Checklist by MindSea Development Inc. (Apr 7, 2013) 
+Xamarin Mobile Application Development for Android by Mark Reynolds (Jan 14, 2014) 
+OpenGL ES 2 for Android: A Quick-Start Guide (Pragmatic Programmers) by Kevin Brothaler (Jul 3, 2013) 
+Android for beginners. Building a Fuel Consumption Calculator with line by line commentary. by Clive Anthony (Jun 2, 2011) 
+Android Apps with App Inventor: The Fast and Easy Way to Build Android Apps by Jörg H. Kloss (Mar 9, 2012) 
+Learning Android Intents by Muhammad Usama bin Aftab and Wajahat Karim (Jan 23, 2014) 
+Beginning Android 3D Game Development by Robert Chin (Mar 11, 2014) 
+Android Security: Attacks and Defenses by Anmol Misra and Abhishek Dubey (Apr 8, 2013) 
+Beginning Android Programming: Develop and Design by Chris Haseman and Kevin Grant (Dec 29, 2013) 
+Voice Application Development for Android by Michael F. McTear and Zoraida Callejas (Nov 25, 2013) 
+Augmented Reality for Android Application Development by Jens Grubert and Dr. Raphael Grasset (Nov 25, 2013) 
+Learning Mobile App Development: A Hands-on Guide to Building Apps with iOS and Android by Jakob Iversen and Michael Eierman (Dec 29, 2013) 
+Robotium Automated Testing for Android by Hrushikesh Zadgaonkar (Nov 21, 2013) 
+Android Application Testing Guide by Diego Torres Milano (Jun 23, 2011) 
+Rapid Android Development: Build Rich, Sensor-Based Applications with Processing by Daniel Sauter (May 2, 2013) 
+Android SQLite and ContentProvider by Lars Vogel (Jan 16, 2012) 
+Practical Android Projects (Books for Professionals by Professionals) by Lucas Jordan and Pieter Greyling (Feb 21, 2011) 
+Android UI Design with XML: Tutorial Book by Mr Camilus Raynaldo (Apr 9, 2012) 
+Android UI Fundamentals: Develop & Design (Develop and Design) by Jason Ostrander (Mar 5, 2012) 
+Android Fragments, Action Bar, Menus, Notifications and Tabs by Clive Sargeant (Jan 28, 2013) 
+Android Apps For Profit: Making Money with Apps on the Android Market by Nathan Mellor (Nov 10, 2013) 
+Developing Android on Android: Automate Your Device with Scripts and Tasks by Mike Riley (Nov 22, 2013) 
+Efficient Android Threading: Asynchronous processing techniques for Android applications by Anders Göransson (May 25, 2014) 
+Asynchronous Android by Steve Liles (Dec 24, 2013) 
+Android Application Security Essentials by Pragati Ogal Rai (Aug 21, 2013) 
+Creating Android Applications: Develop and Design by Chris Haseman (Dec 25, 2011) 
+Learning Android Canvas by Mir Nauman Tahir (Nov 25, 2013) 
+8 Things to Learn Before Making Your App by Dann Berg (Jan 3, 2014) 
+Android Arcade Game App: A Real World Project - Case Study Approach by J. F. DiMarzio (Aug 22, 2012) 
+Making Musical Apps: Real-time audio synthesis on Android and iOS by Peter Brinkmann (Feb 28, 2012) 
